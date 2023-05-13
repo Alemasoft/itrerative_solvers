74b2c9a88a200ee626cf2de3594d45e360bc52ee
Alessandro Maifredi <a.maifredi@campus.unimib.it>
2023-05-13 11:31:15 +0200

    library exports

--- lib/iterative_solvers.dart
+++ lib/iterative_solvers.dart
@@ -3,4 +3,10 @@
 /// More dartdocs go here.
 library iterative_solvers;
 
+export 'src/common/exception.dart';
+export 'src/solver_config.dart';
+export 'src/solvers/coniugate_gradient.dart';
+export 'src/solvers/gauss_seidel.dart';
+export 'src/solvers/gradient.dart';
+export 'src/solvers/jacobi.dart';
 //TODO: Export any libraries intended for clients of this package.
